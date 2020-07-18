61b77f34041e6411186ce1072020a4fe421dbbe9
shivtu <shibendutta@gmail.com>
2020-07-18 11:21:19 +0530

    allow all origins

--- src/main/java/com/example/retail/RetailApplication.java
+++ src/main/java/com/example/retail/RetailApplication.java
@@ -2,12 +2,26 @@ package com.example.retail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RetailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetailApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/*")
+				.allowedOrigins("*");
+			}
+		};
 	}
 
 }
